4cdb27aa7b8573b4c8be60e718a7b10f4aa1f61f
iman <iman@dyneemadev.com>
2023-03-04 12:46:09 +0330

    FIX api v1

--- api.py
+++ api.py
@@ -1,18 +1,40 @@
+from flask import Flask, request, jsonify
 import os
 import subprocess
 
-input_dir = 'videos'
-output_dir = 'audios'
-bitrate = '256'
+app = Flask(__name__)
 
-# Loop over all video files in the input directory
-for filename in os.listdir(input_dir):
-    if filename.endswith('.mp4'):
-        input_file = os.path.join(input_dir, filename)
-        output_file = os.path.join(output_dir, os.path.splitext(filename)[0] + '.mp3')
-        cmd = f'ffmpeg -i {input_file} -vn -acodec libmp3lame -ab {bitrate}k {output_file}'
-        subprocess.run(cmd, shell=True)
-        if os.path.isfile(output_file):
-            print(f'{output_file} was created successfully.')
-        else:
-            print(f'Error creating output file for {input_file}.')
+@app.route('/convert', methods=['POST'])
+def convert_video_to_audio():
+    # Check if the request contains a file
+    if 'file' not in request.files:
+        return jsonify({'message': 'No file in request.'}), 400
+    
+    file = request.files['file']
+    
+    # Check if the file is a video file
+    if file.filename.split('.')[-1] not in ['mp4', 'avi', 'mkv']:
+        return jsonify({'message': 'Invalid file type.'}), 400
+    
+    # Save the file to disk
+    filename = file.filename
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(filepath)
+    
+    # Construct the FFmpeg command
+    output_filename = os.path.splitext(filename)[0] + '.mp3'
+    output_filepath = os.path.join(app.config['UPLOAD_FOLDER'], output_filename)
+    cmd = f'ffmpeg -i {filepath} -vn -acodec libmp3lame -ab 256k {output_filepath}'
+    
+    # Run the FFmpeg command using subprocess
+    subprocess.run(cmd, shell=True)
+    
+    # Check if the output file was created successfully
+    if os.path.isfile(output_filepath):
+        return jsonify({'message': 'Conversion successful.', 'file': output_filename}), 200
+    else:
+        return jsonify({'message': 'Error converting file.'}), 500
+
+if __name__ == '__main__':
+    app.config['UPLOAD_FOLDER'] = 'uploads'
+    app.run(debug=True)
